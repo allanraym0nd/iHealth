3fa678098df972f90c24405d5e53f312e330f6e4
allan raymond <raymonda916@gmail.com>
2025-03-15 19:10:19 +0300

    Fixed routing issues in userRoutes.js

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -3,5 +3,6 @@ const userController = require('../controllers/userController');
 const auth = require('../middleware/auth');
 
 router.get('/users', auth, userController.getUsers);
+router.delete('/users/:id', auth, userController.deleteUser);
 
 module.exports = router;
